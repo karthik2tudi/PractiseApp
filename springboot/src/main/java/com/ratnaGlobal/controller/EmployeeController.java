a04cddac9159f62e07bb14205a7a1896fd4d27f9
Praveen <praveensiliveri100@gmail.com>
2022-04-10 20:18:26 +0530

    added put method by developer2

--- springboot/src/main/java/com/ratnaGlobal/controller/EmployeeController.java
+++ springboot/src/main/java/com/ratnaGlobal/controller/EmployeeController.java
@@ -43,11 +43,16 @@ public class EmployeeController {
 	private Employee update(@RequestBody Employee employee) {
 		empService.saveOrUpdate(employee);
 		return employee;
-
 	}
 
 	@DeleteMapping("deleteemployee/{employeeid}")
 	private void deleteBook(@PathVariable(value = "id") int employeeId) {
 		empService.delete(employeeId);
+	}
+
+                   @PutMapping("/updateemployee")
+	public Employee update(@RequestBody Employee employee) {
+		empService.saveOrUpdate(employee);
+		return employee;
 	}
 }
